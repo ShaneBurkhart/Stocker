1d7f293c9d4c4dacc1c3604b34e688141befc937
ShaneBurkhart <shaneburkhart@gmail.com>
2013-08-22 13:07:57 -0500

    Rough implementation of neuron

---
+++ app/models/neuron.rb
@@ -0,0 +1,37 @@
+class Neuron
+
+  @strategy = nil #determines how to calculate values
+  @output_value = nil #calculated value
+  @inputs = nil #where to get inputs
+  @forward_connections = nil #where to send output
+  @delta = nil #error
+  @bias = nil #random value
+
+  def initialize strategy, inputs
+    @strategy = strategy
+    @inputs = inputs
+  end
+
+  def update_output
+    self.no_strategy_error if !@strategy
+    sum = strategy.calculate_sum @inputs, @bias
+    @output_value = strategy.calculate_output sum
+  end
+
+  def update_free_params
+    self.no_strategy_error if !@strategy
+    @bias = strategy.calculate_bias @bias, @delta
+  end
+
+  def update_delta delta_from_output
+    self.no_strategy_error if !@strategy
+    @delta = strategy.calculate_delta @output, delta_from_output
+  end
+
+  private
+
+    def no_strategy_error
+      raise "No strategy found!"
+    end
+
+end
